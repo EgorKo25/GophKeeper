aa19e5035e8a7c593636d33cd72321d717a58094
EgorKo25 <egorko25@gmail.com>
2023-05-07 03:13:45 +0300

    init

---
+++ internal/server/auth/auth.go
@@ -0,0 +1,115 @@
+package auth
+
+import (
+	"net/http"
+	"time"
+
+	"github.com/EgorKo25/GophKeeper/internal/storage"
+
+	"github.com/dgrijalva/jwt-go"
+)
+
+// Auth is a struct for authentication and session control
+type Auth struct {
+	secretAccessTokenKey  string
+	secretRefreshTokenKey string
+}
+
+// NewAuth is a contractor
+func NewAuth(secretAccessTokenKey string, secretRefreshTokenKey string) *Auth {
+	return &Auth{secretAccessTokenKey: secretAccessTokenKey,
+		secretRefreshTokenKey: secretRefreshTokenKey,
+	}
+}
+
+type claims struct {
+	Name string `json:"name"`
+	jwt.StandardClaims
+}
+
+func (a *Auth) GenerateTokensAndCreateCookie(user *storage.User) ([]*http.Cookie, error) {
+
+	var accessToken, refreshToken string
+	var err error
+
+	accessToken, err = a.generateAccessToken(user)
+	if err != nil {
+		return nil, err
+	}
+
+	refreshToken, err = a.generateRefreshToken(user)
+	if err != nil {
+		return nil, err
+	}
+
+	exp := time.Now()
+
+	cookies := make([]*http.Cookie, 3)
+	cookies[0] = a.getUserCookie(user, exp)
+	cookies[1] = a.getAccessCookie(accessToken, exp)
+	cookies[2] = a.getRefreshCookie(refreshToken, exp)
+
+	return cookies, nil
+
+}
+
+// getRefreshCookie generate access cookie
+func (a *Auth) getRefreshCookie(token string, exp time.Time) *http.Cookie {
+	return &http.Cookie{
+		Name:    "Refresh-token",
+		Value:   token,
+		Expires: exp,
+	}
+}
+
+// getAccessCookie generate access cookie
+func (a *Auth) getAccessCookie(token string, exp time.Time) *http.Cookie {
+	return &http.Cookie{
+		Name:    "Accesses-token",
+		Value:   token,
+		Expires: exp,
+	}
+}
+
+// getUserCookie generate user cookie
+func (a *Auth) getUserCookie(user *storage.User, exp time.Time) *http.Cookie {
+	return &http.Cookie{
+		Name:    "User",
+		Value:   user.Login,
+		Expires: exp,
+	}
+}
+
+// generateToken generate user's jwt token
+func (a *Auth) generateToken(user *storage.User, exp time.Time, secret []byte) (string, error) {
+	cl := &claims{
+		Name: user.Login,
+		StandardClaims: jwt.StandardClaims{
+			ExpiresAt: exp.Unix(),
+		},
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, cl)
+
+	tokenString, err := token.SignedString(secret)
+	if err != nil {
+		return "", err
+	}
+
+	return tokenString, nil
+}
+
+// generateAccessToken generate user's jwt token
+func (a *Auth) generateAccessToken(user *storage.User) (string, error) {
+	exp := time.Now().Add(1 * time.Hour)
+
+	return a.generateToken(user, exp, []byte(a.secretAccessTokenKey))
+}
+
+// generateRefreshToken generate user's jwt token
+func (a *Auth) generateRefreshToken(user *storage.User) (string, error) {
+
+	exp := time.Now().Add(24 * time.Hour)
+
+	return a.generateToken(user, exp, []byte(a.secretRefreshTokenKey))
+}
